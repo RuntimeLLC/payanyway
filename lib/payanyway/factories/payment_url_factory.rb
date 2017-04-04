520eacece98735466baba10b6f07f2d153dbb488
kamsharipova <kam.sharipova@gmail.com>
2017-04-04 14:37:00 +0300

    code review fixes

--- lib/payanyway/factories/payment_url_factory.rb
+++ lib/payanyway/factories/payment_url_factory.rb
@@ -2,7 +2,10 @@ module Payanyway
   module Factories
     class PaymentUrlFactory
       def self.build(params, use_signature)
-        "#{ Payanyway::Gateway.config['payment_url'] }?#{ Payanyway::Factories::QueryParamsFactory.build(params, use_signature) }"
+        [
+          Payanyway::Gateway.config['payment_url'],
+          Payanyway::Factories::QueryParamsFactory.build(params, use_signature)
+        ].join('?')
       end
     end
   end
