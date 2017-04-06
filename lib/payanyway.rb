cbfbefa2a9cdff08afdebc272392839a281ca5a2
nedden <nedden@gmail.com>
2017-04-06 19:08:24 +0530

    'require' fix

--- lib/payanyway.rb
+++ lib/payanyway.rb
@@ -7,6 +7,7 @@ require 'payanyway/helpers/settings'
 require 'payanyway/helpers/signature_generator'
 
 require 'payanyway/factories/payment_url_factory'
+require 'payanyway/factories/widget_url_factory'
 require 'payanyway/request/base'
 require 'payanyway/request/pay'
 require 'payanyway/request/check'
